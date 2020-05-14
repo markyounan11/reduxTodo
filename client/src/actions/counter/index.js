f075280c4dd12d8109516b4e811d4521e981b0ca
Mark Younan <markyounan@gmail.com>
2020-05-14 09:24:49 -0700

    init repo

---
+++ client/src/actions/counter/index.js
@@ -0,0 +1,8 @@
+import { INCREMENT } from '../types';
+
+// These are our action creators
+export const increment = () => {
+    return {
+        type: INCREMENT
+    };
+};
---
+++ client/src/containers/Counter/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Grid, Header, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { increment } from '../../actions/counter';
+
+class Counter extends Component {
+  render() {
+    return (
+      <Grid centered>
+        <Grid.Column textAlign='center'>
+          <Header
+            as='h1'
+            textAlign='center'
+            content='Welcome to the Counter App'
+          />
+          <Header as='h2' textAlign='center'>Counter: <span>{this.props.counter}</span></Header>
+          <Button.Group>
+            <Button
+              icon='minus circle'
+              content='Decrement'
+              negative
+            />
+            <Button.Or/>
+            <Button
+              icon='plus circle'
+              content='Increment'
+              positive
+              onClick={ this.props.increment }
+            />
+          </Button.Group>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { counter: state.counter };
+};
+
+export default  connect(mapStateToProps, { increment })(Counter);
