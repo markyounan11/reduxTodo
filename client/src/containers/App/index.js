19c9db7457fc3fd310b731556250c5f4e2e56e37
Mark Younan <markyounan@gmail.com>
2020-05-16 13:13:59 -0700

    AllTodosList finished

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Grid } from 'semantic-ui-react';
-
+​
 import Counter from '../Counter';
-
+import AllTodosList from '../AllTodosList';
+​
 import Navbar from './../../components/Navbar';
-
+​
 class App extends Component {
   render () {
     return (
@@ -13,10 +14,13 @@ class App extends Component {
         <Grid.Column style={{ maxWidth: 700 }}>
           <Navbar/>
           <Route exact path='/counter' component={Counter}/>
+          <Route exact path='/alltodos' component={AllTodosList}/>
         </Grid.Column>
       </Grid>
     );
   }
 }
-
+​
+​
+​
 export default App;
