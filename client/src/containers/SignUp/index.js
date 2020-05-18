0c8adc57cac68a0e51f5ee396af57d41eb55fee6
Mark Younan <markyounan@gmail.com>
2020-05-18 11:05:56 -0700

    fixed getAllUserEmails function

--- client/src/containers/SignUp/index.js
+++ client/src/containers/SignUp/index.js
@@ -2,37 +2,74 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Form, Segment, Button } from 'semantic-ui-react';
 import { email, length, required } from 'redux-form-validators';
+import axios from 'axios';
 class SignUp extends Component {
-  renderEmail = formProps => {
-    console.log("inside render email", formProps);
-    return (
-      <Form.Input
-        fluid
-        icon='user'
-        iconPosition='left'
-        autoComplete='off'
-        placeholder='Email Address'
-      />
-    );
-  }
-  render() {
-    console.log("Inside of signup render", this.props);
-    return (
-      <Form size='large'>
-        <Segment stacked>
-          <Field
-            name='email'
-            component={ this.renderEmail }
-            validate={
-              [
-                required({ msg: 'Email is required' }),
-                email({ msg: 'You must provide a valid email address' })
-              ]
-            }
-          />
-        </Segment>
-      </Form>
-    );
-  }
+    renderEmail = ({ input, meta }) => {
+        return (
+            <Form.Input
+                {...input}
+                error={meta.touched && meta.error}
+                fluid
+                icon='user'
+                iconPosition='left'
+                autoComplete='off'
+                placeholder='Email Address'
+            />
+        );
+    }
+    renderPassword = ({ input, meta }) => {
+        return (
+            <Form.Input
+                {...input}
+                error={meta.touched && meta.error}
+                fluid
+                type='password'
+                icon='lock'
+                placeholder='password'
+                autoComplete='off'
+                iconPosition='left'
+            />
+        );
+    }
+    render() {
+        console.log("Inside of signup render", this.props);
+        return (
+            <Form size='large'>
+                <Segment stacked>
+                    <Field
+                        name='email'
+                        iscool='mannyiscool'
+                        component={this.renderEmail}
+                        validate={
+                            [
+                                required({ msg: 'Email is required' }),
+                                email({ msg: 'You must provide a valid email address' })
+                            ]
+                        }
+                    />
+                    <Field
+                        name='password'
+                        component={this.renderPassword}
+                        validate={
+                            [
+                                required({ msg: 'You must provide a password' }),
+                                length({ min: 6, msg: 'Your password must be at least 6 characters long' })
+                            ]
+                        }
+                    />
+                </Segment>
+            </Form>
+        );
+    }
 }
-export default reduxForm({ form: 'signup' })(SignUp);
+const asyncValidate = async formValues => {
+    try {
+        const { data } = await axios.get(`/api/user/emails?email=${formValues.email}`);
+        if (data) {
+            throw new Error();
+        }
+    } catch (e) {
+        throw { email: 'Email already exists, please sign up with a different email' };
+    }
+}
+export default reduxForm({ form: 'signup', asyncValidate, asyncChangeFields: ['email'] })(SignUp);
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,8 @@ const { User, Todo } = require('../models/index');
 module.exports = {
   getAllUserEmails: async (req, res) => {
     try {
-      const userEmails = await User.find({}, 'email');
-      return res.status(200).json(userEmails);
+      const userEmail = await User.findOne({ email: req.query.email }, 'email');
+      return res.status(200).json(userEmail);
     } catch (e) {
       return res.status(403).json({ e });
     }
